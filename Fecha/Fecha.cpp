146296e676301639f3b1f858af126f2b58eddb49
joaquinbian <joacobian21@gmail.com>
2024-09-08 16:45:01 -0300

    actividad 5

--- Fecha/Fecha.cpp
+++ Fecha/Fecha.cpp
@@ -1,6 +1,7 @@
 #include "Fecha.h"
+#include <iostream>
 
-
+//constructores
 Fecha::Fecha(){
     this->setFechaDefault();
 }
@@ -15,7 +16,7 @@ Fecha::Fecha(int dia, int mes, int anio){
     }
 }
 
-
+//getters
 int Fecha::getDia(){
     return this->_dia;
 }
@@ -28,14 +29,49 @@ int Fecha::getAnio(){
     return this->_anio;
 }
 
+//setters
+void Fecha::setDia(int dia){
+    if(this->esValidoDia(dia, this->_mes)){
+        this->_dia = dia;
+    } else {
+        this->_dia = 1;
+        std::cout << "El dia no es valido";
+    }
+}
+
+void Fecha::setMes(int mes){
+    if(this->esValidoMes(mes)){
+        this->_mes = mes;
+    } else {
+        this->_mes = 1;
+        std::cout << "El mes no es valido";
+    }
+}
+
+void Fecha::setAnio(int anio){
+    if(anio > 0){
+        this->_anio = anio;
+    } else {
+        this->_anio = anio;
+        std::cout << "El anio no es valido";
+    }
+}
+
+void Fecha::setFechaDefault(){
+    this->_dia = 1;
+    this->_mes = 1;
+    this->_anio = 2023;
+}
+
+
+
+//metodos
 bool Fecha::esFechaValida(int dia, int mes, int anio){
     return this->esValidoDia(dia, mes) && this->esValidoMes(mes);
 }
 
 bool Fecha::esValidoDia(int dia, int mes){
-    int meses[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
-
-    if(meses[mes - 1] < dia){
+    if(_meses[mes - 1] < dia){
         return false;
     } else if(dia < 1 || dia > 31){
         return false;
@@ -52,8 +88,31 @@ bool Fecha::esValidoMes(int mes){
     return true;
 }
 
-void Fecha::setFechaDefault(){
-    this->_dia = 1;
-    this->_mes = 1;
-    this->_anio = 2023;
+
+void Fecha::agregarDia(){
+    if(this->_dia == this->_meses[this->_mes - 1]){
+        if(this->_mes == 12){
+            this->setMes(1);
+            this->setAnio(this->_anio + 1);
+        } else {
+            this->setMes(this->_mes + 1);
+        }
+        this->setDia(1);
+    } else {
+        this->setDia(this->_dia + 1);
+    }
+}
+
+void Fecha::restarDia(){
+    if(this->_dia == 1){
+        if(this->_mes == 1){
+            this->setAnio(this->_anio - 1);
+            this->setMes(12);
+        }else {
+            this->setMes(this->_mes - 1);
+        }
+        this->setDia(this->_meses[this->_mes]);
+    } else {
+        this->setDia(this->_dia - 1);
+    }
 }
--- Fecha/Fecha.h
+++ Fecha/Fecha.h
@@ -7,13 +7,19 @@ public:
     int getDia();
     int getAnio();
     int getMes();
+    void setDia(int dia);
+    void setMes(int mes);
+    void setAnio(int anio);
 
 private:
     int _dia;
     int _mes;
     int _anio;
+    int _meses[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
     bool esFechaValida(int dia, int mes, int anio);
     bool esValidoDia(int dia, int mes);
     bool esValidoMes(int mes);
     void setFechaDefault();
+    void agregarDia();
+    void restarDia();
 };
