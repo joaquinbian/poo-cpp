31b4d65053e390a4586685366e18f9c533eb2a98
joaquinbian <joacobian21@gmail.com>
2024-09-23 20:38:10 -0300

    construccion.cpp

---
+++ Inmobiliaria/Construccion.cpp
@@ -0,0 +1,24 @@
+#include "Inmueble.h"
+#include "Construccion.h"
+#include <iostream>
+
+Construccion::Construccion(int codigo, std::string calle, std::string numero, std::string localidad, std::string nombreDueno, std::string apellidoDueno, std::string DNI, std::string celular, float precioVenta, float precioAlquiler, int antiguedadConstruccion, float superficieTotalM2):Inmueble(codigo, calle, numero, localidad, nombreDueno, apellidoDueno, DNI, celular, precioVenta, precioAlquiler){
+
+
+};
+
+void Construccion::setAntiguedadConstruccion(int antiguedad){
+    if(antiguedad > 0){
+        this->_antiguedadConstruccion = antiguedad;
+    } else {
+        std::cout << "el anio de construccion no puede ser negativo" << std::endl;
+    }
+};
+
+void Construccion::setSuperficieTotalM2(float superficie){
+    if(superficie > 0.0){
+        this->_superficieTotalM2 = superficie;
+    } else{
+        std::cout << "No se puede asignar una superficie menor a 0"<<std::endl;
+    }
+};
--- Inmobiliaria/Construccion.h
+++ Inmobiliaria/Construccion.h
@@ -2,6 +2,7 @@
 #include "Inmueble.h"
 class Construccion : protected Inmueble {
     public:
+        Construccion(int codigo, std::string calle, std::string numero, std::string localidad, std::string nombreDueno, std::string apellidoDueno, std::string DNI, std::string celular, float precioVenta, float precioAlquiler, int antiguedadConstruccion, float superficieTotalM2);
         int getAntiguedadConstruccion();
         float getSuperficieTotalM2();
 
