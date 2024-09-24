4ddfc706b10b9f57113d0635f236b777f1bb9671
joaquinbian <joacobian21@gmail.com>
2024-09-23 21:07:41 -0300

    casa quinta

---
+++ Inmobiliaria/CasaQuinta.cpp
@@ -0,0 +1,17 @@
+#include "CasaQuinta.h"
+#include "Casa.h"
+#include <iostream>
+
+CasaQuinta::CasaQuinta(int codigo, std::string calle, std::string numero, std::string localidad, std::string nombreDueno, std::string apellidoDueno, std::string DNI, std::string celular, float precioVenta, float precioAlquiler, int antiguedadConstruccion, float superficieTotalM2, float superficieCubierta, int habitaciones, bool pileta, bool quincho):Casa(codigo, calle, numero, localidad, nombreDueno, apellidoDueno, DNI, celular, precioVenta, precioAlquiler, antiguedadConstruccion, superficieTotalM2, superficieCubierta, habitaciones){
+    this->setConQuincho(quincho);
+    this->setConPileta(pileta);
+};
+
+
+void CasaQuinta::setConPileta(bool pileta){
+    this->_pileta = pileta;
+}
+
+void CasaQuinta::setConQuincho(bool quincho){
+    this->_quincho = quincho;
+}
--- Inmobiliaria/CasaQuinta.h
+++ Inmobiliaria/CasaQuinta.h
@@ -3,6 +3,7 @@
 
 class CasaQuinta : protected Casa {
     public:
+        CasaQuinta(int codigo, std::string calle, std::string numero, std::string localidad, std::string nombreDueno, std::string apellidoDueno, std::string DNI, std::string celular, float precioVenta, float precioAlquiler, int antiguedadConstruccion, float superficieTotalM2, float superficieCubierta, int habitaciones, bool pileta, bool quincho);
         bool getConPileta();
         bool getConQuincho();
 
